2e01c71262bb7042fc0507c3997eb5c03fcf2839
wandri <wandrille.guesdon@gmail.com>
2017-06-11 13:50:19 +0200

    mise a jour photo

--- app/containers/PhotoPage/index.js
+++ app/containers/PhotoPage/index.js
@@ -14,12 +14,10 @@ const Halogen = require('halogen');
 // import messages from './messages';
 import CameraFile from '../../components/CameraFile';
 import CameraInput from '../../components/CameraInput';
-import {
-  actUploadPicture,
-} from './actions';
+import { actUploadPicture } from './actions';
 
-
-export class PhotoPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export class PhotoPage extends React.PureComponent {
+  // eslint-disable-line react/prefer-stateless-function
   constructor() {
     super();
     this.state = { files: [] };
@@ -31,7 +29,7 @@ export class PhotoPage extends React.PureComponent { // eslint-disable-line reac
     this.setState({
       files: this.state.files[0],
     });
-  }
+  };
 
   render() {
     console.log(this.props.PhotoPage);
@@ -40,21 +38,21 @@ export class PhotoPage extends React.PureComponent { // eslint-disable-line reac
         {this.props.PhotoPage.loading &&
           <div style={styles.loader}>
             <Halogen.PacmanLoader color={'#4DAF7C'} />
-            </div>
-        }
+          </div>}
 
         {!this.props.PhotoPage.loading &&
-        <div>
-          <Card style={styles.pitchCard}>
+          <div className="mainphoto" style={styles.mainphoto}>
+            {/* <Card style={styles.pitchCard}>
             <CardTitle title="Marty" />
             <CardText>Le text de Rita ! </CardText>
-          </Card>
-          <div style={styles.photoButton} >
-            <CameraInput />
-            <CameraFile FileDropped={(file) => this.FileDropped(file)} />
-          </div>
-        </div>
-      }
+          </Card>*/}
+            <div style={styles.photoButton} className="photoButton">
+              <CameraFile FileDropped={(file) => this.FileDropped(file)} />
+            </div>
+            <div style={styles.cameraInput} className="cameraInput">
+              <CameraInput />
+            </div>
+          </div>}
       </div>
     );
   }
@@ -70,13 +68,18 @@ const mapStateToProps = createStructuredSelector({
 
 function mapDispatchToProps(dispatch) {
   return {
-    UploadIdCard: (file) => (dispatch(actUploadPicture(file))),
+    UploadIdCard: (file) => dispatch(actUploadPicture(file)),
   };
 }
 const styles = {
+  mainphoto: {
+    position: 'relative',
+  },
   photoButton: {
+    position: 'absolute',
     textAlign: 'center',
-    margin: '50% auto',
+    left: '42%',
+    top: '375px',
   },
   pitchCard: {
     marginTop: '70px',
@@ -84,6 +87,10 @@ const styles = {
   loader: {
     width: '70px',
     margin: '50% auto',
+  },
+  cameraInput: {
+    position: 'absolute',
+    top: '0px',
   },
 };
 export default connect(mapStateToProps, mapDispatchToProps)(PhotoPage);
